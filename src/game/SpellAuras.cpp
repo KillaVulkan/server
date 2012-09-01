a45c6889bc241b18b77fe4f2b33aa22adc445e12
KillaVulkan <killavulkan@hotmail.de>
2012-09-01 15:55:20 -0700

    Fix some combat behaviour of NPC-Pets

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -2239,9 +2239,7 @@ void Aura::HandleModPossess(bool apply, bool Real)
         if(target->GetTypeId() == TYPEID_UNIT)
         {
             ((Creature*)target)->AIM_Initialize();
-
-            if (((Creature*)target)->AI())
-                ((Creature*)target)->AI()->AttackedBy(caster);
+            target->AttackedBy(caster);
         }
     }
 }
@@ -2425,8 +2423,7 @@ void Aura::HandleModCharm(bool apply, bool Real)
         if(target->GetTypeId() == TYPEID_UNIT)
         {
             ((Creature*)target)->AIM_Initialize();
-            if (((Creature*)target)->AI())
-                ((Creature*)target)->AI()->AttackedBy(caster);
+            target->AttackedBy(caster);
         }
     }
 }
